230f0c99136e056366419a548996e40917ae784b
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-03-07 01:01:27 -0300

    Funcionalidades da Página
    Criação das funcionalidades da página em JavaScript.

---
+++ script.js
@@ -0,0 +1,21 @@
+const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+const senhaInput = document.getElementById("senha");
+const gerarBtn = document.getElementById("gerar");
+const copiarBtn = document.getElementById("copiar");
+
+function gerarSenha() {
+	let senha = "";
+	for (let i = 0; i < 16; i++) {
+		senha += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+	}
+	senhaInput.value = senha;
+}
+
+function copiarSenha() {
+	senhaInput.select();
+	document.execCommand("copy");
+
+}
+
+gerarBtn.addEventListener("click", gerarSenha);
+copiarBtn.addEventListener("click", copiarSenha);
